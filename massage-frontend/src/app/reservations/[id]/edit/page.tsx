43b08c63f6e240f966b032a856633979d1abc6d5
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-11-21 22:12:31 +0700

    add suspense

--- massage-frontend/src/app/reservations/[id]/edit/page.tsx
+++ massage-frontend/src/app/reservations/[id]/edit/page.tsx
@@ -1,9 +1,30 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import EditReservation from "@/components/EditReservation";
+import { getBookingById } from "@/services/reservation/booking.service";
+import { LinearProgress } from "@mui/material";
+import { getServerSession } from "next-auth";
+import { Suspense } from "react";
 
-export default function EditReservationPage({
-  params,
+export default async function EditReservationPage({
+    params,
 }: {
-  params: { id: string };
+    params: { id: string };
 }) {
-  return <><EditReservation/></>;
+    const session = await getServerSession(authOptions);
+    const token = session.user.token;
+    const response = await getBookingById(params.id, token);
+    const booking = response.data;
+
+    return (
+        <>
+            {params.id}
+            <Suspense fallback={<LinearProgress />}>
+                <EditReservation
+                    params={params}
+                    authToken={token}
+                    reservation={booking}
+                />
+            </Suspense>
+        </>
+    );
 }
