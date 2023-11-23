0ecf72859fb9b13f8e671587c0bb1b372ba7d477
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-11-23 09:15:12 +0700

    add service to fetch api

---
+++ massage-frontend/src/services/massage/massage.service.ts
@@ -0,0 +1,56 @@
+import { IMassageBody } from "@/interfaces/massage.interface";
+
+// Function to fetch data from the API
+const API_BASE_URL = `http://localhost:${
+  process.env.BACKEND_PORT || 5000
+}/api/v1`;
+export const getShops = async (token: string) => {
+  try {
+    const res = await fetch(`${API_BASE_URL}/shops`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+
+export const postShop = async (shopData: IMassageBody, token: string) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/shops`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`, // Assuming you need to be authorized to post
+      },
+      body: JSON.stringify(shopData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data; // The newly created shop data
+  } catch (error) {
+    // If the error is not an instance of Error, create a new Error
+    if (!(error instanceof Error)) {
+      throw new Error('An unknown error occurred');
+    }
+    // Log the error message and rethrow the error
+    console.error(error.message);
+    throw error;
+  }
+};
